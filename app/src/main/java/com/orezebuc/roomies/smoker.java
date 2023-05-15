f9255de5aba80b1d96b2796f488b6d57feaa653f
bhavin <bhavinparmar1953@gmail.com>
2023-05-15 10:17:45 +0530

    Change in activity_first_bio1.xml, added activity_two_field_of_study.xml, activity_three_smoker.xml, activity_four_drink.xml, activity_five_sleeping.xml, activity_six_food.xml, activity_seven_religion.xml and also added class file of that xml file..
    class file for activity two study, for activity three smoker.java, for activity four drink.java, for activity five sleeping.java, for activity six food.java, for activity seven religion.java and also change in AndroidManifest.xml file..

---
+++ app/src/main/java/com/orezebuc/roomies/smoker.java
@@ -0,0 +1,14 @@
+package com.orezebuc.roomies;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class smoker extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_three_smoker);
+    }
+}
