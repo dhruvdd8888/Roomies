4cb84b20548ed6e34b1359d6f6eba7463b1784d5
dhruv <dhruvdd8888@gmail.com>
2023-08-13 22:40:39 +0530

    users scroll

--- app/src/main/java/com/orezebuc/roomies/religion.java
+++ app/src/main/java/com/orezebuc/roomies/religion.java
@@ -1,14 +1,35 @@
 package com.orezebuc.roomies;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class religion extends AppCompatActivity {
-
+    TextView hindu, muslim, other;
+    int selected;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seven_religion);
+
+        hindu = findViewById(R.id.Hindu);
+        muslim = findViewById(R.id.Muslim);
+        other = findViewById(R.id.other);
+
+        hindu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selected = 0;
+                Intent intent = new Intent(religion.this, homeScreen.class);
+                religion.this.startActivity(intent);
+
+            }
+        });
+
     }
+
+
 }
